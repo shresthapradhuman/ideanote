89a53aa47e1131c35bb34060592c39f0214151b4
shresthapradhuman <shresthapradhuman2018@gmail.com>
2023-12-27 00:41:54 +0900

    configure

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains: ["res.cloudinary.com"],
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "res.cloudinary.com",
-        pathname: "**",
+        hostname: "**",
       },
     ],
   },
