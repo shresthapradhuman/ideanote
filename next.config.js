192b5a0eefc15b0c54ab9e1306608e4d5ddc5185
shresthapradhuman <shresthapradhuman2018@gmail.com>
2023-12-27 00:38:53 +0900

    configured cloudinary

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["res.cloudinary.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "**",
+      },
+    ],
   },
   headers: async () => {
     return [
@@ -20,10 +26,6 @@ const nextConfig = {
             key: "Access-Control-Allow-Headers",
             value:
               "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-          {
-            key: "Content-Security-Policy",
-            value: "default-src 'self'",
           },
         ],
       },
