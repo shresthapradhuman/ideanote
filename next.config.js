539ed0566c744383259b0707e4191db7ced37934
shresthapradhuman <shresthapradhuman2018@gmail.com>
2023-12-27 01:50:30 +0900

    reverse

--- next.config.js
+++ next.config.js
@@ -1,23 +1,12 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        hostname: "**",
-      },
-    ],
+    domains: ["res.cloudinary.com"],
   },
   headers: async () => {
     return [
       {
-        source: "/((?!api|_next/static|_next/image|favicon.ico).*)",
-        missing: [
-          { type: "header", key: "next-router-prefetch" },
-          { type: "header", key: "purpose", value: "prefetch" },
-        ],
+        source: "/api/:path*",
         headers: [
           {
             key: "Access-Control-Allow-Origin",
@@ -31,18 +20,6 @@ const nextConfig = {
             key: "Access-Control-Allow-Headers",
             value:
               "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "X-XSS-Protection",
-            value: "1; mode=block",
           },
         ],
       },
