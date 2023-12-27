ac5969aa39f3965ce00adaf423ac69114c4b563c
shresthapradhuman <shresthapradhuman2018@gmail.com>
2023-12-27 23:09:47 +0900

    cleanup

--- app/(root)/notes/[slug]/_component/notetitle.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const NoteTitle = ({ title }: { title: string }) => {
-  return <h1 className="text-lg md:text-5xl md:leading-normal">{title}</h1>;
-};
-
-export default NoteTitle;
--- app/dashboard/_components/comment-detail.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CommentDetail = () => {
-  return <div>CommentDetail</div>;
-};
-
-export default CommentDetail;
